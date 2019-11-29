5b9d5a719f8b6f43b68500e6c87ccc68b9ce6f22
youshumin <youshumin@teeqee.com>
2019-11-29 09:43:18 +0800

    去除检查权限GET放,仅仅支持POST方法

--- handlers/api.py
+++ handlers/api.py
@@ -1,7 +1,7 @@
 '''
 @Author: your name
 @Date: 2019-11-28 10:05:17
-@LastEditTime: 2019-11-28 17:32:00
+@LastEditTime: 2019-11-29 09:41:59
 @LastEditors: Please set LastEditors
 @Description: In User Settings Edit
 @FilePath: /rbac/handlers/api.py
@@ -12,7 +12,8 @@ import logging
 from oslo.web.requesthandler import MixinRequestHandler
 from oslo.web.route import route
 from tornado.gen import coroutine
-from utils.auth import get_user_info_bytoken, api_check_permission
+
+from utils.auth import api_check_permission, get_user_info_bytoken
 
 LOG = logging.getLogger(__name__)
 
@@ -44,26 +45,6 @@ class checkPermissionHandler(MixinRequestHandler):
             return
         check_permission = api_check_permission(self, check_path, check_method)
 
-        if check_permission:
-            self.send_ok(data="")
-            return
-        self.send_fail(msg="没有权限")
-        return
-
-    @coroutine
-    def get(self):
-        req_data = self.request_body()
-        check_path = req_data.get("check_path", [])
-        check_auth = req_data.get("check_auth", [])
-        if not check_auth or not check_path:
-            self.send_fail(msg="没有权限")
-            return
-        auth_info = get_user_info_bytoken(check_auth)
-        self.user_id = auth_info["userId"]
-        if not self.user_id:
-            self.send_fail(msg="没有权限")
-            return
-        check_permission = api_check_permission(self, check_path)
         if check_permission:
             self.send_ok(data="")
             return
